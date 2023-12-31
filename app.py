f44c86bf29de80b3c7614871433eb55b65902ba5
Klaire Curde <kcurde@gmail.com>
2023-12-30 22:59:34 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,54 @@
+import os
+import sys
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+# Initializes your app with your bot token and socket mode handler
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+# Listens to incoming messages that contain "hello"
+# To learn available listener arguments,
+# visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    # say(f"Hey there <@{message['user']}>!")
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "Click Me"},
+                    "action_id": "button_click"
+                }
+            }
+        ],
+        text=f"Hey there <@{message['user']}>!"
+    )
+
+@app.action("button_click")
+def action_button_click(body, ack, say):
+    # Acknowledge the action
+    ack()
+    say(f"<@{body['user']['id']}> clicked the button")
+
+@app.command("/enroll")
+def add_to_blacklist(ack, respond, command):
+    ack()
+    respond(f"{command['text']}")
+
+@app.command("/leave")
+def remove_from_blacklist(ack, respond, command):
+    ack()
+    respond(f"{command['text']}")
+
+
+# Start your app
+if __name__ == "__main__":
+    try:
+        SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+    except KeyboardInterrupt:
+        sys.exit()
+
