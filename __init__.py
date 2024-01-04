b2e6295a62459c9373b082075af59c5859557600
Klaire Curde <kcurde@gmail.com>
2024-01-03 23:54:49 -0500

    basic flow for checking if any mentioned users had kevlar enabled

--- __init__.py
+++ __init__.py
@@ -14,6 +14,13 @@ usr_ptrn = re.compile('@[\S]*')
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
+# In order to not get rate limited by Slack, we must fetch the id of the custom field we want manually.
+# See https://api.slack.com/methods/users.profile.get#arg_include_labels
+field_id = ""
+for field in app.client.team_profile_get()['profile']['fields']:
+    if field["label"] == "Kevlar":
+        field_id = field["id"]
+
 # Listens to incoming messages that contain "hello"
 # To learn available listener arguments,
 # visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
@@ -41,7 +48,6 @@ def message_hello(message, say):
     # "subtype": "channel_join"
 })
 def detect_mention(event, say, body):
-    say(f'{body["event"]["text"]}')
     matches = usr_ptrn.findall(body["event"]["text"])
     if matches:
         say(f"{list(match.strip('@>') for match in matches)}")
@@ -51,16 +57,18 @@ def detect_mention(event, say, body):
     mentions = list(match.strip('@>') for match in matches)
     # say(f"{mentions[0]}")
     # pp.pp(f"{app.client.team_profile_get()['profile']['fields']}")
-    field_id = ""
-    for field in app.client.team_profile_get()['profile']['fields']:
-        if field["label"] == "Kevlar":
-            field_id = field["id"]
 
     # pp.pp(f"{app.client.team_profile_get()}", depth=1, width=20)
     # say(f"{app.client.users_profile_get(user=mentions[0])}")
-    say(f"{app.client.users_profile_get(user=mentions[0])['profile']['fields'][field_id]['value']}")
-    # say(f"{app.client.users_profile_get(user=mentions[0])['fields']}")
-    # for uid in mentions:
+    for uid in mentions:
+        try:
+            if app.client.users_profile_get(user=uid)['profile']['fields'][field_id]['value'] == "true":
+                # Kevlar is set, proceed to delete message
+                pass
+            say(f"{app.client.users_profile_get(user=uid)['profile']['fields'][field_id]['value']}") 
+        except KeyError:
+            # Kevlar not set
+            pass
 
 
 # Start your app
