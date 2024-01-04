aa5d7c4b30cdbb76eeb035009910acf23dc2ee62
Klaire Curde <kcurde@gmail.com>
2024-01-04 14:51:17 -0500

    can now delete messages of any mentioned user with kevlar enabled

--- __init__.py
+++ __init__.py
@@ -6,53 +6,38 @@ import pprint as pp
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-import psycopg
-
 # This pattern matches any string starting with an '@'.
 usr_ptrn = re.compile('@[\S]*')
 
-# Initializes your app with your bot token and socket mode handler
+# Initializes app with a bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
-# In order to not get rate limited by Slack, we must fetch the id of the custom field we want manually.
+# In order to not get rate limited by Slack, we must dynamically fetch the id of the custom field we want manually.
 # See https://api.slack.com/methods/users.profile.get#arg_include_labels
 field_id = ""
 for field in app.client.team_profile_get()['profile']['fields']:
     if field["label"] == "Kevlar":
         field_id = field["id"]
 
-# Listens to incoming messages that contain "hello"
-# To learn available listener arguments,
-# visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
-@app.message("hello")
-def message_hello(message, say):
-    # say() sends a message to the channel where the event was triggered
-    # say(f"Hey there <@{message['user']}>!")
-    say(
-        blocks=[
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
-                "accessory": {
-                    "type": "button",
-                    "text": {"type": "plain_text", "text": "Click Me"},
-                    "action_id": "button_click"
-                }
-            }
-        ],
-        text=f"Hey there <@{message['user']}>!"
-    )
+
+# Deletes the provided message in the provided channel
+def deflect(channel, ts):
+    app.client.chat_delete(channel=channel, ts=ts)
 
 @app.event({
     "type": "message",
     # "subtype": "channel_join"
 })
 def detect_mention(event, say, body):
+    # Messages of certain subtypes do not contain text, thus we need to terminate early
+    if "text" not in body["event"]:
+        return
     matches = usr_ptrn.findall(body["event"]["text"])
     if matches:
         say(f"{list(match.strip('@>') for match in matches)}")
     else:
-        say("No matches.")
+        # Message does not "@" anyone, so ignore
+        return
 
     mentions = list(match.strip('@>') for match in matches)
     # say(f"{mentions[0]}")
@@ -64,8 +49,11 @@ def detect_mention(event, say, body):
         try:
             if app.client.users_profile_get(user=uid)['profile']['fields'][field_id]['value'] == "true":
                 # Kevlar is set, proceed to delete message
-                pass
-            say(f"{app.client.users_profile_get(user=uid)['profile']['fields'][field_id]['value']}") 
+                say("Deleting message.")
+                deflect(channel=event["channel"], ts=event["ts"])
+                say("Done.")
+                break
+                # say(f"{app.client.users_profile_get(user=uid)['profile']['fields'][field_id]['value']}")
         except KeyError:
             # Kevlar not set
             pass
